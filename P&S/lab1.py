fe82a4805a59e9e1830af36f13edcd115fe614aa
abhi8127 <abhisheky06880@gmail.com>
2026-01-01 23:21:25 +0530

    lab1

---
+++ P&S/lab1.py
@@ -0,0 +1,18 @@
+# To print something
+print("Hello world")
+
+# Define a list
+mylist=[2,3,'MTech']
+
+# To append any other element into a list
+mylist.append('EMC5103')
+
+# You want to add at specific place
+mylist.insert(0,'5103')
+
+# To delete item from the list
+del mylist[4]
+
+# To empty this list
+mylist.clear()
+print(mylist)
